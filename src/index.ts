02ae55dbfa0237e7ad662906d528dcad433d5f16
mahammed-siyabuddeen <mohdsihab615@gmail.com>
2024-08-11 07:42:14 +0530

    home page package

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,25 @@ import signup from './routes/signup'
 import login from './routes/login'
 import googleSuccessAth from './routes/googleSuccessAuth'
 import product from './routes/product'
-import { json } from 'stream/consumers';
+import cart from './routes/cart.routes'
+import order from './routes/orders.router'
 import mongoose from 'mongoose';
 import env from 'dotenv';
 import multer from 'multer';
 import { v2 as cloudinary } from 'cloudinary'
-import { error, log } from 'console';
-import { body, validationResult } from 'express-validator';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import cors from 'cors'
+import { cloudinaryconfig } from './utils/cloudinaryConfig'
+import paymentRouter from './routes/payment.router'
+
+
+const port: number = 9000
+
+
+env.config()
+const app = express();
+app.use(express.json())
+app.use(cors())
+cloudinaryconfig
 
 cloudinary.api.ping((error, result) => {
   if (error) {
@@ -21,24 +31,31 @@ cloudinary.api.ping((error, result) => {
   }
 });
 
-const port: number = 9000
-
-
-const app = express();
-app.use(express.json())
-app.use(cors())
-env.config()
 app.get('/', (req: Request, res: Response) => {
   console.log(process.env.API_KEY);
-
   res.send("hello");
 })
-
-
 app.use('/signup', signup)
 app.use('/login', login)
 app.use('/product', product)
-app.use('/google-auth',googleSuccessAth)
+app.use('/google-auth', googleSuccessAth)
+app.use('/cart', cart)
+app.use('/order', order)
+app.use('/payment', paymentRouter)
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './public')
+  },
+  filename: (req, file, cb) => {
+    const uniqe = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.originalname)
+  }
+})
+const upload = multer({ storage });
+
+
 
 
 
