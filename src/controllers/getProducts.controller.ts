767bda4d58aca012521043286277aafa27dd7dfc
mahamme-siyabuddeen <mohd.sihab615@gmail.com>
2024-08-14 23:20:32 +0530

    search Product

--- src/controllers/getProducts.controller.ts
+++ src/controllers/getProducts.controller.ts
@@ -1,16 +1,27 @@
 import { Request, Response } from "express";
 import { productModel } from "../models/Product";
 import { Error } from "mongoose";
+import mongoose from "mongoose";
 
 
-export const getproducts=async(req:Request,res:Response)=>{
+export const getproducts = async (req: Request, res: Response) => {
 
     try {
-        
-        const products=await productModel.find().sort({"created_at":1}).limit(10).exec()
-        res.status(200).json(products)
-    } catch (error:unknown) {
-        if(error instanceof Error)
+        const { name, category } = req.query;
+        console.log(req.query);
+        let products;
+        if (name) {
+             products = await productModel.find({
+                $text: { $search: (name as string) }
+            }).sort({ score: { $meta: "textScore" } }).limit(10).exec()
+        }
+        else if(category){
+           products=await productModel.find({category_id:new mongoose.Types.ObjectId(category as string)})
+            .limit(10).exec();
+        }
+        return  res.status(200).json(products)
+    } catch (error: unknown) {
+        if (error instanceof Error)
             return res.status(400).send(error.message)
         res.status(400).send("something wrong")
     }
