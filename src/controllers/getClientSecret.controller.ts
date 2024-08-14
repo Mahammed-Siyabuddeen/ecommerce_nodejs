d888e9679069b34c1141a3f3b876f169b28ba077
mahamme-siyabuddeen <mohd.sihab615@gmail.com>
2024-08-14 23:19:57 +0530

    payment and webHook orders

--- src/controllers/getClientSecret.controller.ts
+++ src/controllers/getClientSecret.controller.ts
@@ -3,8 +3,9 @@ import Stripe from "stripe";
 
 export const getClientSecret = async (req: Request, res: Response) => {
     try {
+       
         const stripe = new Stripe('sk_test_51PlwiKGBMIgRPdYIW7uCxvuoPgPv8hDDe6fRr4s3d95Sj3AzQglp6MU7UPXcYwTcwztlsQ42JuVrT395neBKpMa500pciZTsna')
-        const { amount, cart_id, user_id } = req.body;
+        const { amount, cart_id, user_id,name, street,city,zipcode,state,country,phone } = req.body;
         console.log(req.body);
         const paymentIntents = await stripe.paymentIntents.create({
             amount: amount,
@@ -14,6 +15,13 @@ export const getClientSecret = async (req: Request, res: Response) => {
                 user_id: user_id,
                 cart_id: cart_id,
                 amount: amount,
+                name,
+                street,
+                city,
+                zipcode,
+                state,
+                country,
+                phone
             }
         });
         console.log('get client id successfully');
--- src/controllers/webHook.controller.ts
+++ src/controllers/webHook.controller.ts
@@ -4,39 +4,72 @@ import { orderModel } from "../models/order.model";
 import { cartItemModel } from "../models/cartItem.model";
 import { productModel } from "../models/Product";
 import mongoose from "mongoose";
+import { orderItemsModel } from "../models/orderItems.model";
+import { addressModel } from "../models/address.model";
 
 export const webHook = async (req: Request, res: Response) => {
+  console.log('webhook comming');
   const endpointSecrect = 'whsec_47e85aae365be541f520655b2865b40664942b8df4d2eca90a86522c94ba1047'
   const sig = req.headers['stripe-signature'] as string | undefined
   let event: Stripe.Event;
-  // try {
-  //   // event = stripe.webhooks.constructEvent(req.body.id, sig as string, endpointSecrect)
-  // } catch (error) {
-  //   console.log(`Webhook Error: ${error instanceof Error ? error.message : error}`);
-  //   return res.status(400).send(`Webhook Error: ${error}`);
-  // }
+
   switch (req.body.type) {
     case 'payment_intent.succeeded':
       const paymentIntent = req.body.data.object;
       // console.log();
 
-      console.log('PaymentIntent was successful!', paymentIntent);
+      console.log('PaymentIntent was successful!',);
       // Fulfill the purchase, e.g., update the order in your database
 
-      const cartItems = await cartItemModel.find({ cart_id: paymentIntent.metadata.cart_id })
-      const prouducts = await productModel.find({
-        _id: {
-          $in:[cartItems.map((item:any)=>new mongoose.Types.ObjectId(item.product_id))]
-        }
-      },{_id:1,price:1}).exec()
+      try {
+        const { user_id, cart_id, name, street, city, state, country, zipcode, phone } = paymentIntent.metadata;
+        const addressData = await addressModel.create({
+          name,
+          street,
+          city,
+          state,
+          country,
+          zipcode,
+          phone,
+          user_id: new mongoose.Types.ObjectId(user_id)
+        })
+        const orderData = orderModel.create({ user_id, total_amount: paymentIntent.amount, address_id: addressData._id });
+        const data = cartItemModel.aggregate([
+          {
+            $match: { cart_id: new mongoose.Types.ObjectId(cart_id) },
+          },
+          {
+            $lookup: {
+              from: "products",
+              localField: "product_id",
+              foreignField: "_id",
+              as: "products"
+            }
+          },
+          {
+            $unwind: '$products'
+          },
+          {
+            $project: {
+              _id: 0,
+              product_id: 1,
+              quantity: 1,
+              price: '$products.price'
+            }
+          }
+        ])
+        const Promisedata = await Promise.all([orderData, data, addressData]);
+        const orderData_id = Promisedata[0]._id;
+        const orderItemData = Promisedata[1].map((item) => { return { ...item, order_id: orderData_id } });
+        orderItemsModel.insertMany(orderItemData)
+        console.log('hee data', Promisedata);
 
-      const data=cartItemModel.aggregate([
-        {
-          $match:{cart_id:paymentIntent.metadata.cart_id}
-        }
-      ])
-
-      console.log(cartItems);
+        res.status(200).json(orderItemData);
+      } catch (error) {
+        console.log('switch catch error',error);
+        
+        res.status(400).json(error)
+      }
 
       break;
     // Handle other event types as needed
