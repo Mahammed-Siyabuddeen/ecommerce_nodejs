a95d1c3ec91efefba1fdd4acd10c55b751060890
mahamme-siyabuddeen <mohd.sihab615@gmail.com>
2024-08-14 23:21:16 +0530

    orders

--- src/routes/orders.router.ts
+++ src/routes/orders.router.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import { orderProduct } from "../controllers/orderProduct.controller";
+import orderProductById from "../controllers/orderProductById.controller";
+import { getAllOrders } from "../controllers/getAllOrders.controller";
 
 const router=Router();
 
 router.post('/',orderProduct);
+router.get('/admin/getorders',getAllOrders)
+router.get('/:order_id/:orderitem_id',orderProductById)
 export default router;
