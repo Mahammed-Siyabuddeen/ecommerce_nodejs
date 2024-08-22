a3eb282ef2569e4fc07e2aa3c0c2854fde645427
mahamme-siyabuddeen <mohd.sihab615@gmail.com>
2024-08-22 21:38:37 +0530

    order

---
+++ src/controllers/cancelOrder.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { reviewModel } from "../models/review.model";
+import mongoose from "mongoose";
+import { categoryModel } from "../models/category.model";
+import { orderModel } from "../models/order.model";
+
+export const cancelOrder = async (req: Request, res: Response) => {
+    try {
+       const {order_id}=req.body;
+       await orderModel.findOneAndUpdate({_id:order_id},{$set:{status:'canceled'}});
+       res.status(204).send();
+    }
+    catch (error: unknown) {
+        if (error instanceof Error)
+            return res.status(400).send(error.message)
+        res.status(400).send("something wrong")
+    }
+}
